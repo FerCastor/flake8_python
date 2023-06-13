0cbb00cdbc1bf8addc73f32c8b852c5a3e8d0e7a
Fernanda Castor <102315480+FerCastor@users.noreply.github.com>
2023-06-13 16:56:43 -0300

    criando mais tipagens

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-from fila_normal import FilaNormal
-from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
-fila_teste = FilaNormal()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-print(fila_teste.chama_cliente(5))
-print(fila_teste.chama_cliente(10))
-print(fila_teste.chama_cliente(2))
-fila_teste2 = FilaPrioritaria()
-fila_teste2.atualiza_fila()
-fila_teste2.atualiza_fila()
-fila_teste2.atualiza_fila()
-print(fila_teste2.chama_cliente(10))
-fila_teste2.atualiza_fila()
-print(fila_teste2.chama_cliente(15))
-print(fila_teste2.estatistica('10/10/2010', 215, 'detail'))
+teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(3))
